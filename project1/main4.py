587454bcf4637de6323a55c2f43d1931017a9cf0
Teun Domburg <t.h.domburg@student.tudelft.nl>
2025-11-19 12:33:05 +0100

    cleaned up prints

--- project1/main4.py
+++ project1/main4.py
@@ -70,11 +70,7 @@ while index < no_of_primes+10:
         if binary_exponents not in seen:
             seen.add(binary_exponents)
             index += 1
-            #print(index)
     k, j = increment(k, j)
-#print(x_is, [x*x % N for x in x_is])
-#print(smooth_numbers)
-#print(max(x_is))
 
 with open("project1/input.txt", "w") as file:
     file.write(f"{no_of_primes+10} {no_of_primes}")
@@ -84,8 +80,6 @@ with open("project1/input.txt", "w") as file:
 
 
 result = subprocess.run(["./project1/gauss.out", "project1/input.txt", "project1/output.txt"], capture_output=True)
-#print(result.stdout)
-#print(result.stderr)
 
 with open("project1/output.txt", "r") as file:
     no_of_solutions = int(file.readline())
@@ -95,26 +89,18 @@ with open("project1/output.txt", "r") as file:
     for line in file.readlines():
         solutions[i] = np.array(list(map(int, line.split())))
         i += 1
-#print(len(solutions[0]))
-#print(solutions)
 
 x_solutions = [1 for _ in range(no_of_solutions)]
 for i in range(no_of_solutions):
     for j in range(no_of_primes+10):
         x_solutions[i] = (x_solutions[i] * int(pow(x_is[j], int(solutions[i][j])))) % N
-    #print(i)
 
 y_solutions = [1 for _ in range(no_of_solutions)]
 for i in range(no_of_solutions):
     total_exponents = np.sum(smooth_numbers * solutions[i][:, np.newaxis], axis=0)
-    #print(total_exponents)
     for j in range(no_of_primes):
         y_solutions[i] = y_solutions[i] * int(pow(factorbase[j], int(total_exponents[j])//2, N)) % N
-    #print(i)
-#print(x_is)
-#print(x_solutions)
-#print("\n\n\n")
-#print(y_solutions)
+
 
 for i in range(no_of_solutions):
     a = gcd(N, x_solutions[i]+y_solutions[i])
