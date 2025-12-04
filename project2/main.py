46c885070bbdc0a311cbe758087921a542a2cc96
Kasper Johansson <kasperjohansson27@gmail.com>
2025-12-04 12:00:29 +0100

    Refactor clock logic and update loop iterations

--- project2/main.py
+++ project2/main.py
@@ -10,14 +10,8 @@ class LFSR:
         self.coefficients = coefficients
     
     def clock(self) -> int:
-
-        new = -np.sum(self.coefficients * self.state) % self.mod
-
-        #implement logic machine from exercise HE5
-        if self.mod == 2:
-            nonlinear = 1-bool(self.state[1] | self.state[2] | self.state[3])
-            new = (new ^ nonlinear)
-        
+        new = (-np.sum(self.coefficients*self.state) + 
+               (self.state[0]+1)*int(not (self.state[3] >= 1 or self.state[2] >= 1 or self.state[1] >= 1))) % self.mod
         to_return = self.state[0]
         self.state = np.roll(self.state, -1)
         self.state[-1] = new
@@ -29,13 +23,13 @@ def de_bruijn(base: int, exp: int):
 
     if base == 2 and exp == 4:
         LFSR_2 = LFSR(base, exp, np.array([0, 0, 0, 1]), np.array([1, 0, 0, 1])) # x^4 + x + 1
-        for _ in range(base**exp):
+        for _ in range(10**4+3):
             nums.append(LFSR_2.clock())
-
+    
 
     elif base == 5 and exp == 4:
         LFSR_5 = LFSR(base, exp, np.array([0, 0, 0, 1]), np.array([2, 0, 2, 1])) # 2x^4 + 2x^2 + x + 1
-        for _ in range(base**exp):
+        for _ in range(10**4+3):
             nums.append(LFSR_5.clock())
         
     return nums
@@ -49,9 +43,9 @@ if __name__ == "__main__":
     nums = []
 
     for i in range(10**4+3):
-        nums.append(5*seq_2[i%len(seq_2)]+seq_5[i%len(seq_5)])  
+        nums.append(5*seq_2[i%(2**4)]+2*seq_5[i%(5**4)])  
         if i >= 3:
-            codes.add(str(nums[i]) + str(nums[i-1]) + str(nums[i-2]) + str(nums[i-3])) 
+            codes.add(str(nums[i]) + str(nums[i-1]) + str(nums[i-2]) + str(nums[i-3]))  
 
     print("\n".join(map(str, nums)))
     print(len(codes))
